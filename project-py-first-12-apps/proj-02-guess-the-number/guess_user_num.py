671cf0def35f2aabcebe9e2e0b4ed43565a02741
Anuj Upadhyay <openmindedlearning@gmail.com>
2023-12-20 20:58:07 +0530

    project-py-01-first-12-apps - 02 - guess the number.

---
+++ project-py-first-12-apps/proj-02-guess-the-number/guess_user_num.py
@@ -0,0 +1,19 @@
+import random
+
+
+def guess(x):
+    random_num = random.randint(1, x)
+    guess = 0
+
+    while guess != random_num:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+
+        if guess < random_num:
+            print("Too low!")
+        elif guess > random_num:
+            print("Too high!")
+
+    print(f"You guessed it, the number is {random_num} !!  Alright...")
+
+
+guess(10)
