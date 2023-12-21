8b26d6ad3b452f00677e2dd51cda1f507613f220
Anuj Upadhyay <openmindedlearning@gmail.com>
2023-12-21 13:28:33 +0530

    project-py-01-first-12-apps - 02 - guess the number user com.

--- project-py-first-12-apps/proj-02-guess-the-number/guess_user_num.py
+++ project-py-first-12-apps/proj-02-guess-the-number/guess_user_num.py
@@ -1,6 +1,7 @@
 import random
 
 
+# user's guess
 def guess(x):
     random_num = random.randint(1, x)
     guess = 0
@@ -16,4 +17,28 @@ def guess(x):
     print(f"You guessed it, the number is {random_num} !!  Alright...")
 
 
-guess(10)
+# user's guess
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+
+    while feedback != "c" and low != high:
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+
+        feedback = input(
+            f"Is {guess} too high (H), too low (L), or correct (C)? "
+        ).lower()
+
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
+
+    print(f"Wow! The computer guessed your number, {guess}, correctly!")
+
+
+computer_guess(100)
